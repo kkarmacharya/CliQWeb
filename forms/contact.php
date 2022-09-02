376d9ed92520f5b690291f65ba65074d535c0527
Kshitiz <nepal.teamlead@asterinnovations.com>
2022-09-02 17:23:29 +0545

    payments flow

--- forms/contact.php
+++ forms/contact.php
@@ -2,14 +2,14 @@
   /**
   * Requires the "PHP Email Form" library
   * The "PHP Email Form" library is available only in the pro version of the 
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
+  * The library should be uploaded to: vendor/cliq-form/cliq-form.php
+  * For more info and help: https://bootstrapmade.com/cliq-form/
   */
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'contact@example.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = '../assets/vendor/cliq-form/cliq-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
